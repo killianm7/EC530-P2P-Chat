1406bb09932a37d1093f06ba536b74605576cb99
killianm7 <killianm@bu.edu>
2023-04-09 23:32:57 -0400

    New closing option to end chat

--- client.py
+++ client.py
@@ -5,13 +5,19 @@ import threading
 def receive_messages(server_socket):
     while True:
         msg = server_socket.recv(1024)
+        if not msg or msg.decode() == "X":
+            break
         # encrypted_msg = server_socket.recv(1024)
         # decrypted_msg = decrypt_message(encrypted_msg)
         print(f'\nReceived: {msg.decode()}\n')
+    server_socket.close()
+    print("Connection has closed. Have a good day!")
 
 def main():
     host = input("Enter the server IP address: ")
     port = int(input("Enter the port #:"))
+
+    print("Type 'X' in the chat to close the connection.")
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.connect((host, port))
@@ -20,9 +26,13 @@ def main():
 
     while True:
         message = input("Enter your message: ")
-        # encrypted_msg = encrypt_message(message)
         server_socket.send(message.encode())
+        if message == "X":
+            break
+        # encrypted_msg = encrypt_message(message)
         # print(f'Message: {message}')
+    
+    print("Ending connection...")
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 import socket
 import threading
+from pymongo import MongoClient
+from datetime import datetime
 # from encryption import encrypt_message, decrypt_message
 
 connected_clients = []
 
+client = MongoClient('mongodb+srv://killianm:VXnjHFL9nRTAFaYF@cluster0.tgc659u.mongodb.net/test')
+
 def handle_client(client_socket, addr):
     while True:
         msg = client_socket.recv(1024)
-        if not msg:
+        if not msg or msg.decode() == "X":
             break
+
         # encrypted_msg = client_socket.recv(1024)
         # if not encrypted_msg:
         #     break
@@ -23,8 +28,18 @@ def handle_client(client_socket, addr):
 
         print(f"Message from {addr}: {msg}")
 
+        upload_to_mongodb("P2P", "Messages", {"Message": msg.decode()}, {"Address": addr}, {"Time (EST)": datetime.now()})
+
     client_socket.close()
     connected_clients.remove(client_socket)
+    print(f"Connection from {addr} has closed.")
+
+def upload_to_mongodb(database_name, collection_name, documents, user_info, date):
+    db = client[database_name]
+    collection = db[collection_name]
+
+    result = collection.insert_one({**documents, **user_info, **date})
+    # print(f"Inserted {result.inserted_id} messages into {database_name}.{collection_name}")
 
 def main():
     host = input("Enter the server IP address: ")
